3cea0372babb44dfa8fabb82e31e1ff3b1f56e7a
Julius DeAngelis <juliusdeangelis@gmail.com>
2022-07-04 12:05:19 -0400

    attempt 7

--- backend/server.js
+++ backend/server.js
@@ -12,9 +12,9 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('frontend/build'));
-}
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('frontend/build'));
+// }
 
 app.use('/api/transfers', require('./routes/transferRoutes'))
 app.use('/api/users', require('./routes/userRoutes'))
